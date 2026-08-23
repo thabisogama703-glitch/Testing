40c3f26fd011c5bb26f0392cad55afd7a987e83d
thabisogama703-glitch <thabisogama703@gmail.com>
2026-08-23 19:52:56 +0200

    Question 2/Login/Registration system

---
+++ Question2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Question2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtModuleName = new System.Windows.Forms.TextBox();
+            this.listModules = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtModuleName
+            // 
+            this.txtModuleName.Location = new System.Drawing.Point(26, 42);
+            this.txtModuleName.Name = "txtModuleName";
+            this.txtModuleName.Size = new System.Drawing.Size(306, 20);
+            this.txtModuleName.TabIndex = 0;
+            // 
+            // listModules
+            // 
+            this.listModules.FormattingEnabled = true;
+            this.listModules.Location = new System.Drawing.Point(26, 138);
+            this.listModules.Name = "listModules";
+            this.listModules.Size = new System.Drawing.Size(306, 277);
+            this.listModules.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(371, 42);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(119, 38);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add Module";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(371, 167);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(119, 38);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "Sort Module";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(371, 108);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(119, 38);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search Module";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(518, 42);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(119, 38);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Remove Module";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter Module Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Search a module";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(26, 97);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(306, 20);
+            this.txtSearch.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listModules);
+            this.Controls.Add(this.txtModuleName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtModuleName;
+        private System.Windows.Forms.ListBox listModules;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ Question2/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> modules = new List<string>();
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string module_name = txtModuleName.Text;
+
+            if (string.IsNullOrWhiteSpace(module_name))
+            {
+                MessageBox.Show("Please provide a module name!", "Missing Value",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                return;
+            }
+
+            if (modules.Contains(module_name))
+            {
+                MessageBox.Show("Module already exist", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            // Adding module to the list (backend)
+            modules.Add(module_name);
+
+
+            // Trying to display the updated list in the listbox (Frontend)
+            listModules.Items.Clear();
+
+            foreach (string A_module in modules)
+            {
+                listModules.Items.Add(A_module);
+            }
+
+            txtModuleName.Clear();
+
+            txtModuleName.Focus();
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listModules.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a module to be removed!", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            // Remove from the list<> the specific selected module
+            string selected_module = listModules.SelectedItem.ToString();
+            modules.Remove(selected_module);
+
+            // TRying to display the fresh updated list<> in the listbox
+            listModules.Items.Clear();
+
+            foreach (string single_module in modules)
+            {
+                // Adding a module individually to the listbox
+                listModules.Items.Add(single_module);
+            }
+
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string module_name = txtSearch.Text;
+
+            if (string.IsNullOrWhiteSpace(module_name))
+            {
+                MessageBox.Show("Please provide a module name!", "Missing Value",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                return;
+            }
+
+            int index = modules.IndexOf(module_name);
+
+            if (index != -1)
+            {
+                MessageBox.Show($"The module is found at position {index}");
+            }
+            else
+            {
+                MessageBox.Show("Module not found!");
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            modules.Sort();
+
+            // TRying to display the fresh updated list<> in the listbox
+            listModules.Items.Clear(); 
+
+            foreach (string single_module in modules)
+            {
+                // Adding a module individually to the listbox
+                listModules.Items.Add(single_module);
+            }
+
+        }
+    }
+}
